6e9f3cfa5b5227708c25669f944a6bb3e3d8118c
GST <tourtelt@gmail.com>
2025-06-12 21:07:34 +0300

    Small change

--- useful_commands.sh
+++ useful_commands.sh
@@ -1,3 +1,5 @@
+# Spot Instance
+
 # Get list of instances with their state, spot request id, and state transition reason
 aws ec2 describe-instances \
     --filters "Name=tag:Name,Values=carla-rl" \
@@ -18,3 +20,21 @@ aws ec2 describe-instances \
 
 # Then SSH in and watch the cloud-init log
 ssh ubuntu@<INSTANCE_IP> 'tail -f /var/log/cloud-init-output.log'
+
+####################
+# On Demand Instance
+
+# Get the instance ID and public IP
+aws ec2 describe-instances \
+    --filters "Name=tag:Name,Values=carla-rl" "Name=instance-state-name,Values=pending,running" \
+    --query 'Reservations[].Instances[].[InstanceId,PublicIpAddress,State.Name]' \
+    --output table
+
+# Once you have the IP, monitor cloud-init in real-time
+ssh ubuntu@<INSTANCE_IP> 'tail -f /var/log/cloud-init-output.log'
+
+# Or check cloud-init status
+ssh ubuntu@<INSTANCE_IP> 'cloud-init status'
+
+# Check if cloud-init encountered any errors
+ssh ubuntu@<INSTANCE_IP> 'sudo cat /var/log/cloud-init.log | grep -i error'
